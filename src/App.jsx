db85c7fe1a200cbab34443c1fa7299ec6af88ba6
jgj1020 <s2433@e-mirim.hs.kr>
2026-06-06 23:12:09 +0900

    1 빼기 기능 추가 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
-import { useState } from 'react'; // 1. 상태 바구니 꺼내오기
+import { useState } from 'react';
 
 function App() {
-  // 2. [숫자 담긴 바구니, 숫자를 바꿀 때 쓰는 함수] = useState(처음 시작할 숫자)
-  const [number, setNumber] = useState(0); 
-
+  // 1. 기본값이 0인 전광판 상자 세트 만들기
+  const [number, setNumber] = useState(0);
+  <h1>Go React!</h1>
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>🔢 나의 첫 리액트 카운터</h1>
-      
-      {/* 3. 화면에 바구니 안에 있는 숫자를 보여주기 */}
-      <h2 style={{ fontSize: '40px' }}>{number}</h2>
-      
-      {/* 4. 버튼을 누르면(onClick), setNumber 함수를 써서 숫자를 +1 하기 */}
-      <button onClick={() => setNumber(number + 1)} style={{ padding: '10px 20px', fontSize: '18px' }}>
+      {/* 2. 전광판 화면에 띄우기 */}
+      <h1 style={{ fontSize: '40px' }}>{number}</h1>
+
+      {/* 3. 1 더하기 버튼 */}
+      <button 
+        onClick={() => setNumber(number + 1)} 
+        style={{ padding: '10px 20px', fontSize: '18px', marginRight: '10px' }}
+      >
         1 더하기
+      </button>
+
+      {/* 4. 방금 직접 만드신 멋진 1 빼기 버튼! */}
+      <button 
+        onClick={() => setNumber(number - 1)} 
+        style={{ padding: '10px 30px', fontSize: '18px' }}
+      >
+        1 빼기
       </button>
     </div>
   );
